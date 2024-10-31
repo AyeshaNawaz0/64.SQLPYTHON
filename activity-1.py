90dc705a6e100bfd3ec67a922e02ed24bec32de3
AyeshaNawaz0 <zawanahseya@gmail.com>
2024-10-31 17:54:03 +0500

    first commit

---
+++ activity-1.py
@@ -0,0 +1,7 @@
+print("What type of above datebase is?\n 1.relational database \n 2.non-relational database")
+ans = int(input("Enter your guess : "))
+if ans == 2:
+    print("you guessed right")
+else :
+    print("your guess is wrong")
+print("please tell your mentor why wou guessed this?")
